e582b634884badaf657ac526a0271e1f6e0faf2a
Sjoerd van der Heide <sjoerd.vd.heide@hotmail.com>
2018-04-22 21:58:39 +0100

    Added R scripts

---
+++ MovieBuffBot/resources/R/numbers.R
@@ -0,0 +1,39 @@
+# install.packages("stringr")
+# install.packages("ggplot2")
+
+
+library(RMySQL)
+library(ggplot2)
+
+con <- dbConnect(MySQL(), dbname="bigmovieher", user="root", password="", host="localhost")
+
+b <- "'% "
+e <- " %'"
+numbername <- c("One","Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine")
+
+
+
+#Add values of query to name
+for (n in c("One","Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" )){
+  ns <- paste0(b, n, e)
+  cn <- dbGetQuery(con, sprintf("SELECT count(primaryTitle) FROM  title WHERE name LIKE %s", ns))
+  nam <- paste(n)
+  assign(nam, cn)
+  
+}
+
+#Make dataframe for barplot
+df <- data.frame()
+#names(df) <- c("Number", "Count")
+numbers <- c(One ,Two, Three, Four, Five, Six, Seven, Eight, Nine)
+for (i in 1:9){
+    de <- data.frame(numbername[i],numbers[i])
+    names(de) <- c("Number", "Count")
+    df <- rbind(df, de)
+}
+
+#Plot graphs
+library("ggplot2")
+library("plyr")
+ggplot(data=df, aes(x=Number,y=Count)) + geom_bar(stat="identity")
+ggsave(filename = "numberTitles.jpeg")
---
+++ MovieBuffBot/resources/R/professionPie.R
@@ -0,0 +1,27 @@
+#install.packages("RMySQL")
+#install.packages("plotrix")
+
+#Wat is de verdeling van de verschillende soorten beroepen die met films te maken hebben? Laat dit zien in een pie chart
+#"Welke beroepen zijn er in de film business?"
+
+library(RMySQL)
+library(ggplot2)
+library(ggrepel)
+library(scales)
+library(plotly)
+
+con <- dbConnect(MySQL(), dbname="bigmovieher", user="root", password="", host="localhost")
+
+professionCount <- dbGetQuery("SELECT profession, count(profession) FROM bigmovieher.name_profession WHERE profession NOT LIKE \"\" GROUP BY profession HAVING count(profession) > 2 ORDER BY count(profession) desc")
+
+
+#get the names and slices as a separate variable
+lbls <- professionCount[,2]
+slices <- professionCount[,1]
+
+
+bp <- ggplot(professionCount, aes(x="", y=slices, fill=lbls)) + geom_bar(width = 1, stat = "identity") + ggtitle("Professions in the movie business")
+pie <- bp + coord_polar("y", start = 0)
+png(filename = "professionPie.png")
+pie + geom_bar(stat = "identity", color = 'black') + guides(fill=guide_legend(override.aes = list(colour=NA)))
+dev.off()
